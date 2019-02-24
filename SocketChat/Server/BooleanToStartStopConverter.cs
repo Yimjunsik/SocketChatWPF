c1100b19a0e9edc7d9bbce6c6017e533d3b5417d
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-02-24 21:16:43 +0900

    Server 프로젝트 내에, BooleanToStartStopConverter 클래스 작성 완료

---
+++ SocketChat/Server/BooleanToStartStopConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Server
+{
+    public class BooleanToStartStopConverter : IValueConverter
+    {
+        private const string strTrue = "Stop";
+        private const string strFalse = "Start";
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+                if ((bool)value)
+                    return strTrue;
+            return strFalse;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.ToString() == strTrue;
+        }
+    }
+}
