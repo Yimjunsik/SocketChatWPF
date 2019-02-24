a17d12e47b6c9e80db962994fa541e2b5f22ea9a
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-02-24 15:10:44 +0900

    ChatServer 클래스 내 최초 멤버변수, Propertychanged 이벤트 핸들러 선언 및 Port , IsServerActive 를 위한 Set, Get 메소드 작성

--- SocketChat/Server/ChatServer.cs
+++ SocketChat/Server/ChatServer.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.ComponentModel;
 
 namespace Server
 {
-    class ChatServer
+    public class ChatServer
     {
+        private Dispatcher _dispatcher { get; set; }
+
+        private int _clientIdCounter;
+        private Thread _thread;
+        private Socket _socket;
+
+        private IPAddress _ipAddress;
+        private ushort _port;
+        public ushort Port
+        {
+            get
+            {
+                return _port;
+            }
+            set
+            {
+                if (this.IsServerActive)
+                    throw new Exception("Can't change this property when server is active");
+                this._port = value;
+            }
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propName) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        #endregion
+
+        private bool _isServerActive;
+        public bool IsServerActive
+        {
+            get
+            {
+                return _isServerActive;
+            }
+            private set
+            {
+                this._isServerActive = value;
+
+                this.NotifyPropertyChanged("IsServerActive");
+                this.NotifyPropertyChanged("IsServerStopped");
+            }
+        }
     }
 }
