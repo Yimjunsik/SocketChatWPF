2402e2844355c7cd455ea86747b2d98cb729e6e1
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-02-24 21:29:11 +0900

    Server 프로젝트 내, BooleanToServerStatusMessageConverter 클래스 작성 완료

---
+++ SocketChat/Server/BooleanToServerStatusMessageConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Server
+{
+    public class BooleanToServerStatusMessageConverter : IValueConverter
+    {
+        private const string strTrue = "Server is active";
+        private const string strFalse = "Server is stopped";
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is bool)
+                if ((bool)value)
+                    return strTrue;
+            return strFalse;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value.ToString() == strTrue;
+        }
+    }
+
+}
