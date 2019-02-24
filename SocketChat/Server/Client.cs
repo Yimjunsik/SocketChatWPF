f47bdd960f4b4c3dedf1626a71bdc51258465535
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-02-24 15:53:01 +0900

    Server 프로젝트 / Client 클래스 내 _id, _username 멤버변수, PropertyChanged 이벤트 선언 및 ID를 위한 Set, Get 메소드 작성 완료

--- SocketChat/Server/Client.cs
+++ SocketChat/Server/Client.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel;
 
 namespace Server
 {
-    public class Client
+    public class Client : INotifyPropertyChanged
     {
+        private int _id;
+        private string _username;
 
+        public int ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                this._id = value;
+                this.NotifyPropertyChanged("ID");
+            }
+        }
+
+        #region INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propName) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        #endregion
     }
 }
