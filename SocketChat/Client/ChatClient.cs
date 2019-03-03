8bb5f019d9144d41d22a296bc3e7160362463e12
DESKTOP-FE0M562\yjs3694 <yjs3694@gmail.com>
2019-03-03 20:29:01 +0900

    ChatClient 클래스 내에 멤버변수 _dispatcher, _thread, _socket 추가

--- SocketChat/Client/ChatClient.cs
+++ SocketChat/Client/ChatClient.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Client
 {
     public class ChatClient
     {
+        private Dispatcher _dispatcher;
+        private Thread _thread;
+        private Socket _socket;
+
+
     }
 }
